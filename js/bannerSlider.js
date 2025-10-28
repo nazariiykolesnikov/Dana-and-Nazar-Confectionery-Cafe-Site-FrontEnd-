8046333233116012c3ac9c7537b5390e4ab91cef
Nazar Kolesnikov <nazarkolesnikov148@ukr.net>
2025-10-28 05:15:52 +0200

    Implement automatic and manual slide navigation

--- js/bannerSlider.js
+++ js/bannerSlider.js
@@ -1,6 +1,44 @@
-jQuery(document).ready(function($) {
-    $('.slider-image').on('click', function() {
-        $('.slider-image').removeClass('active');
-        $(this).addClass('active');
+$(document).ready(function () {
+    const $slides = $('#heroSlider .slide');
+    let currentIndex = 0;
+    const slideCount = $slides.length;
+    const intervalTime = 5000; // 5 секунд
+    let autoSlide;
+
+    function showSlide(index) {
+        $slides.removeClass('active').fadeOut(600);
+        $slides.eq(index).addClass('active').fadeIn(600);
+    }
+
+    function nextSlide() {
+        currentIndex = (currentIndex + 1) % slideCount;
+        showSlide(currentIndex);
+    }
+
+    function prevSlide() {
+        currentIndex = (currentIndex - 1 + slideCount) % slideCount;
+        showSlide(currentIndex);
+    }
+
+    $('#nextSlide').on('click', function () {
+        nextSlide();
+        resetAutoSlide();
     });
+
+    $('#prevSlide').on('click', function () {
+        prevSlide();
+        resetAutoSlide();
+    });
+
+    function startAutoSlide() {
+        autoSlide = setInterval(nextSlide, intervalTime);
+    }
+
+    function resetAutoSlide() {
+        clearInterval(autoSlide);
+        startAutoSlide();
+    }
+
+    $slides.hide().eq(currentIndex).show().addClass('active');
+    startAutoSlide();
 });
